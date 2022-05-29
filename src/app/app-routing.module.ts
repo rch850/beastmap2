1659ac965bd4c2c3dbaa39c97ea159d295dc6725
rch850 <rich850@gmail.com>
2022-05-29 14:42:06 +0900

    👍 About ページを追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { GameComponent } from './game/game.component';
 
 const routes: Routes = [
-  { path: '', component: GameComponent }
+  { path: '', component: GameComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
