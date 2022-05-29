631925842d33e6347f25201cb17ef027822e9f19
rch850 <rich850@gmail.com>
2022-05-30 02:10:39 +0900

    ♻️  Refactor

---
+++ src/app/answer-button/answer-button.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-answer-button',
+  templateUrl: './answer-button.component.html',
+})
+export class AnswerButtonComponent {
+  @Input() name = ''
+  @Input() imageUrl = ''
+}
