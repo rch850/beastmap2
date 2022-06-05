b1f527fb8ed1d32a4a9b65324ce5a13536881531
rch850 <rich850@gmail.com>
2022-06-06 00:04:32 +0900

    正解不正解をボタンで表示

--- src/app/answer-button/answer-button.component.ts
+++ src/app/answer-button/answer-button.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+
+export type AnswerState = 'empty' | 'correct' | 'wrong'
 
 @Component({
   standalone: true,
   selector: 'app-answer-button',
+  imports: [
+    CommonModule
+  ],
   templateUrl: './answer-button.component.html',
 })
 export class AnswerButtonComponent {
   @Input() name = ''
   @Input() imageUrl = ''
+  @Input() state: AnswerState = 'empty'
 }
