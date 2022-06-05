4fec4befaf8e413a4b3131368cabba3d0558b84e
rch850 <rich850@gmail.com>
2022-06-05 16:20:02 +0900

    🚿 tidy up imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { GameComponent } from './game/game.component';
 
 const routes: Routes = [
   { path: '', loadComponent: () => import('./game/game.component').then(m => m.GameComponent) },
