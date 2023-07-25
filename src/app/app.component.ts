91d37651ac7620b2f2727f1a049442cff6f0cc21
rch850 <rich850@gmail.com>
2023-07-26 02:06:48 +0900

    ng generate @angular/core:standalone step 2, 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass'],
+    standalone: true,
+    imports: [RouterOutlet]
 })
 export class AppComponent {
   title = 'beastmap2';
