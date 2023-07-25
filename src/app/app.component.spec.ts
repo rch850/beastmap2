2f7e4c38b9eae523902de9e1f9aa22bb8458abff
rch850 <rich850@gmail.com>
2023-07-25 23:31:52 +0900

    🚿 不要なテストを削除

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,10 +17,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'beastmap2'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('beastmap2');
-  });
 });
